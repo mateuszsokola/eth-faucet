3c6da3c5a345ffae688ed18658276458de774f31
mateusz <mateusz@stake.fish>
2022-08-12 11:14:56 +0200

    feat: update open source memo

--- components/OpenSourceMemo.tsx
+++ components/OpenSourceMemo.tsx
@@ -1,25 +1,14 @@
-import { Typography, Link as MuiLink } from "@mui/material"
+import { Link as MuiLink } from "@mui/material"
 import Link from "next/link"
 import { RoundedBox } from "./RoundedBox"
 
 export const OpenSourceMemo = () => (
   <RoundedBox>
-    <b>Would you like to build similar app?</b> Feel free to browse code or watch the entire tutorial on YouTube:
-    <ul>
-      <li>
-        <Link href="https://github.com/mateuszsokola/eth-faucet" passHref>
-          <MuiLink target="_blank" rel="noopener referrer">
-            Source Code (Github)
-          </MuiLink>
-        </Link>
-      </li>
-      <li>
-        <Link href="https://www.youtube.com/channel/UCJV16_5c4A0amyBZSI4yP6A" passHref>
-          <MuiLink target="_blank" rel="noopener referrer">
-            Tutorial (YouTube video)
-          </MuiLink>
-        </Link>
-      </li>
-    </ul>
+    <b>Would you like to build similar app?</b> Feel free to browse the&nbsp;
+    <Link href="https://github.com/mateuszsokola/eth-faucet" passHref>
+      <MuiLink target="_blank" rel="noopener referrer">
+        source code on Github
+      </MuiLink>
+    </Link>
   </RoundedBox>
 )
