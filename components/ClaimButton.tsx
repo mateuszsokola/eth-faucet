5991a562b22b95c0bc3e2f6949dbb7e3930a3566
Mateusz Sokola <mateusz@stake.fish>
2022-08-12 10:06:04 +0200

    feat: refactor claim button out of the home page (#13)

---
+++ components/ClaimButton.tsx
@@ -0,0 +1,78 @@
+import { Button } from "@mui/material"
+import LoadingButton from "@mui/lab/LoadingButton"
+import { Goerli, useEthers } from "@usedapp/core"
+import { isNil } from "lodash"
+import Link from "next/link"
+import { hasMetamask } from "../hooks/hasMetamask"
+import { claimTokens, retrieveNonce } from "../services/HttpClient"
+import { messageTemplate } from "../utils/textMessage"
+
+type ClaimButtonProps = {
+  onSuccess: () => void
+  onError: (message: string) => void
+}
+
+export const ClaimButton = ({ onSuccess, onError }: ClaimButtonProps) => {
+  const { account, library, isLoading: loading, activateBrowserWallet, switchNetwork, chainId } = useEthers()
+  const installed = hasMetamask()
+
+  const claimGorliEth = async () => {
+    try {
+      if (isNil(library) || isNil(account)) {
+        throw new Error("Wallet is not connected")
+      }
+
+      const nonce = await retrieveNonce()
+      const message = messageTemplate(nonce)
+
+      const signer = library.getSigner()
+      const signature = await signer.signMessage(message)
+
+      await claimTokens(account as string, message, signature)
+      onSuccess()
+    } catch (e: any) {
+      if (e.name === "AxiosError" && e.response.data.message) {
+        onError(e.response.data.message)
+        return
+      }
+
+      onError(e?.message || "Something went wrong")
+    }
+  }
+
+  if (!installed) {
+    return (
+      <Link href="https://metamask.io/download/" passHref>
+        <Button variant="contained" fullWidth>
+          Install MetaMask
+        </Button>
+      </Link>
+    )
+  }
+
+  if (loading) {
+    return <LoadingButton variant="contained" loading fullWidth />
+  }
+
+  if (!account) {
+    return (
+      <Button variant="contained" onClick={() => activateBrowserWallet()} fullWidth>
+        Connect wallet
+      </Button>
+    )
+  }
+
+  if (chainId !== Goerli.chainId) {
+    return (
+      <Button variant="contained" onClick={() => switchNetwork(Goerli.chainId)} fullWidth>
+        Switch to Görli network
+      </Button>
+    )
+  }
+
+  return (
+    <Button variant="contained" onClick={claimGorliEth} fullWidth>
+      Claim Görli ETH
+    </Button>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,92 +1,76 @@
 import type { NextPage } from "next"
 import { formatEther } from "@ethersproject/units"
-import { Button } from "@mui/material"
-import LoadingButton from "@mui/lab/LoadingButton"
-import { Goerli, useEtherBalance, useEthers } from "@usedapp/core"
-import { useCallback, useState } from "react"
-import { isNil } from "lodash"
-import Link from "next/link"
+import { useEtherBalance, useEthers } from "@usedapp/core"
+import { useCallback, useReducer } from "react"
 import { Alert } from "../components/Alert"
+import { ClaimButton } from "../components/ClaimButton"
 import { Item } from "../components/Item"
 import { RoundedBox } from "../components/RoundedBox"
-import { hasMetamask } from "../hooks/hasMetamask"
 import { useWalletClassification } from "../hooks/useWalletClassification"
-import { claimTokens, retrieveNonce } from "../services/HttpClient"
-import { messageTemplate } from "../utils/textMessage"
+
+type Action =
+  | {
+      type: "success"
+    }
+  | {
+      type: "default"
+    }
+  | {
+      type: "error"
+      error: string
+    }
+
+type State =
+  | {
+      status: "success"
+    }
+  | {
+      status: "default"
+    }
+  | {
+      status: "error"
+      error: string
+    }
+
+const initialState: State = {
+  status: "default"
+}
+
+const reducer = (_: State, action: Action): State => {
+  switch (action.type) {
+    case "error":
+      return { status: "error", error: action.error }
+    case "success":
+      return { status: "success" }
+    default:
+      return { status: "default" }
+  }
+}
 
 const Home: NextPage = () => {
-  const { account, library, isLoading: loading, activateBrowserWallet, switchNetwork, chainId } = useEthers()
-  const [success, setSuccess] = useState<boolean>(false)
-  const [error, setError] = useState<string | undefined>(undefined)
-  const installed = hasMetamask()
+  const { account } = useEthers()
+  const [state, dispatch] = useReducer(reducer, initialState)
   const balance = useEtherBalance(account, { refresh: "everyBlock" })
   const [retrieveAmount] = useWalletClassification()
 
-  const claimGorliEth = async () => {
-    try {
-      setError(undefined)
+  const handleSuccess = () => dispatch({ type: "success" })
 
-      if (isNil(library) || isNil(account)) {
-        throw new Error("Wallet is not connected")
-      }
+  const handleError = (error: string) => dispatch({ type: "error", error })
 
-      const nonce = await retrieveNonce()
-      const message = messageTemplate(nonce)
-
-      const signer = library.getSigner()
-      const signature = await signer.signMessage(message)
-
-      await claimTokens(account as string, message, signature)
-      setSuccess(true)
-    } catch (e: any) {
-      setSuccess(false)
-
-      if (e.name === "AxiosError" && e.response.data.message) {
-        setError(e.response.data.message)
-        return
-      }
-
-      setError(e?.message || "Something went wrong")
+  const renderAlert = useCallback(() => {
+    switch (state.status) {
+      case "success":
+        return (
+          <Alert severity="success">
+            Görli ETH has been dispatched to your wallet. You should receive it within 3 minutes.
+          </Alert>
+        )
+      case "error":
+        return <Alert severity="error">{state.error}</Alert>
+      default:
+        return null
     }
-  }
-
-  const renderButton = useCallback(() => {
-    if (!installed) {
-      return (
-        <Link href="https://metamask.io/download/" passHref>
-          <Button variant="contained" fullWidth>
-            Install MetaMask
-          </Button>
-        </Link>
-      )
-    }
-
-    if (loading) {
-      return <LoadingButton variant="contained" loading fullWidth />
-    }
-
-    if (!account) {
-      return (
-        <Button variant="contained" onClick={() => activateBrowserWallet()} fullWidth>
-          Connect wallet
-        </Button>
-      )
-    }
-
-    if (chainId !== Goerli.chainId) {
-      return (
-        <Button variant="contained" onClick={() => switchNetwork(Goerli.chainId)} fullWidth>
-          Switch to Görli network
-        </Button>
-      )
-    }
-
-    return (
-      <Button variant="contained" onClick={claimGorliEth} fullWidth>
-        Claim Görli ETH
-      </Button>
-    )
-  }, [loading, account, chainId, installed])
+  }, [state.status])
 
   return (
     <RoundedBox>
@@ -98,13 +82,8 @@ const Home: NextPage = () => {
         <span>Claimable Görli ETH</span>
         <span>{formatEther(retrieveAmount(account))} ETH (testnet)</span>
       </Item>
-      {renderButton()}
-      {success && !error && (
-        <Alert severity="success">
-          Görli ETH has been dispatched to your wallet. You should receive it within 3 minutes.
-        </Alert>
-      )}
-      {!success && error && <Alert severity="error">{error}</Alert>}
+      <ClaimButton onSuccess={handleSuccess} onError={handleError} />
+      {renderAlert()}
     </RoundedBox>
   )
 }
